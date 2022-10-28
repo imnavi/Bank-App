aea87130d97a875ec5f59f3c3cde270164cd6e58
Navaneeth M P <4navaneeth007@gmail.com>
2022-10-28 12:59:12 +0530

    27-10

---
+++ bank-serve/services/registerservice.js
@@ -0,0 +1,71 @@
+const jwt = require('jsonwebtoken')
+const datab = require('./db')
+
+const register = (accno, name, pswd) => {
+    return datab.User.findOne({ accno: accno }).then(user => {
+        if (user) {
+            return {
+                statusCode: 422,
+                status: false,
+                message: "already exist"
+            }
+        }
+        else {
+            const newUser = new datab.User({
+                accno,
+                name,
+                pswd,
+                balance: 0,
+                transaction: []
+            })
+            newUser.save()
+            currentName = name
+            currentAccount = accno
+            currentBalance = 0
+            const token = jwt.sign({
+                accountNo: accno
+            }, 'supersecretkey@123')
+            return {
+                statusCode: 200,
+                status: true,
+                message: "registered successfully",
+                token,
+                currentName,
+                currentAccount,
+                currentBalance
+            }
+        }
+    })
+}
+
+const signIn = (accountNo, password) => {
+    return datab.User.findOne({ accno: accountNo, pswd: password }).then(item => {
+        console.log("item", item)
+        if (item) {
+            currentName = item.name
+            currentAccount = accountNo
+            currentBalance = item.balance
+            const token = jwt.sign({
+                accountNo: accountNo
+            }, 'supersecretkey@123')
+
+            return {
+                statusCode: 200,
+                status: true,
+                message: "login successfully",
+                token,
+                currentName,
+                currentAccount,
+                currentBalance
+            }
+        }
+        else {
+            return {
+                statusCode: 422,
+                status: false,
+                message: "invalid username or password"
+            }
+        }
+    })
+}
+module.exports = { register, signIn }
